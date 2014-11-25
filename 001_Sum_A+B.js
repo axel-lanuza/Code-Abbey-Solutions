ec3a7c3c824edcf59f8cd302de28ab4d1ee009c4
Justin <senne.justin@gmail.com>
2014-11-25 11:43:54 -0500

    Create 001_Sum_A+B.js

---
+++ 001_Sum_A+B.js
@@ -0,0 +1,9 @@
+//Instructions
+
+//You are to add two numbers and tell their sum. Though you can do it manually, try to write a simple program in any programming language you know or like or want to learn.
+
+//create a function that takes two arguments and adds them
+function sums(a, b) {
+    alert(a + b);
+}
+sums(6294325, 6018160);
